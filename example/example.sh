0e6d3aa28eb3ae302e16aeb2b8cdd5eed94f256e
Ben Winding <hello@benwinding.com>
2021-08-28 15:31:13 +1000

    Better examples

--- example/example.sh
+++ example/example.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 # Make sure you've installed according to README in folder above
 
-pdfstamp stamp  -i ./sample.pdf -s ./signature2.png -l 100 -b 100 -z 25 -p 1
+pdfstamp stamp --debug -i ./sample.pdf -s ./signature2.png -l 100 -b 100 -z 25 -p 1 -o ./output-bl.pdf
+pdfstamp stamp --debug -i ./sample.pdf -s ./signature2.png -r 10 -t 10 -z 25 -p 1 -o ./output-tr.pdf
